91530fda05c0285828ee9c1000ebe1f5471b180c
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-05-08 16:15:54 +0900

    docs: update project description

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Jflask
-> ❄️ Jflask: Framework for Flask RESTful API and management
+> ❄️ Jflask: Template for Flask RESTful API and management
 
 ## Server
 >  RESTful API server with a systematic structure separated by resources for rapid development
--- server/namespaces/__init__.py
+++ server/namespaces/__init__.py
@@ -17,7 +17,7 @@ def hash_password(password):
 api = Api(
     title='Jflask',
     version='1.0',
-    description='Framework for Flask RESTful API and management',
+    description='Template for Flask RESTful API and management',
 )
 
 from server.namespaces.test import test_ns
