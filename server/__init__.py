467ff3bbd12ed5a51e7ff0a5e9c19a68b601bb7b
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-05-21 01:33:19 +0900

    fix: remove flask_principal

--- server/__init__.py
+++ server/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_common import Common
 from flask_cors import CORS
-from flask_principal import Principal
 from flask_pymongo import PyMongo
 from flask_jwt_extended import JWTManager
 import os
@@ -13,7 +12,6 @@ def create_app():
     _app = Flask(__name__)
 
     CORS().init_app(_app)
-    Principal().init_app(_app)
     JWTManager().init_app(_app)
     _app.secret_key = os.urandom(24)
     _app.config.from_object(DevConfig)
